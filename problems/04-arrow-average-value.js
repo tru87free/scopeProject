345f690a07a659d67b161d95e55c40d63727a2a5
tru87free <Tru9787free@gmail.com>
2022-10-06 16:30:22 -0500

    04 arrow

--- problems/04-arrow-average-value.js
+++ problems/04-arrow-average-value.js
@@ -9,7 +9,16 @@ arrowAvgValue([10, 20]); // => 15
 arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
+const arrowAvgValue = (array) => {
+  let avg = 0
 
+for (let i = 0; i < array.length; i++){
+  let el = array[i]
+  avg += el
+}
+
+  return avg / array.length
+}
 // Your code here
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
